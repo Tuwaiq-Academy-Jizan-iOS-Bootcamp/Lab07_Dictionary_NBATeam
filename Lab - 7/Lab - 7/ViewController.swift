08123ea5472edb31216d178d6a6ffc304554dac5
Afrah Omar <afrahomar@MacBook-Pro-Afrah.local>
2021-10-18 09:43:15 +0300

    lab07

---
+++ Lab - 7/Lab - 7/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Lab - 7
+//
+//  Created by Afrah Omar on 12/03/1443 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    let team1 = ["Curry":"Golden State Warriors","GThomson":"Golden State Warriors","Green":"Golden State Warriors"]
+        let team2 = ["Lebron":"Lakers","Westbrook":"Lakers","Davis":"Lakers"]
+        let team3 = ["Leornard":"Clippers","Batum":"Clippers","Ibaka":"Clippers"]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
