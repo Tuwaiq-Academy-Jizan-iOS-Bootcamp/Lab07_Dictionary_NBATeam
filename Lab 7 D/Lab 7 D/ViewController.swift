c1bf0b3dd9273c48117a444b298cf9971d80c77f
layla hakami <laylahakami@laylas-MacBook-Air.local>
2021-10-18 09:16:04 +0300

    lab dectionary

---
+++ Lab 7 D/Lab 7 D/ViewController.swift
@@ -0,0 +1,45 @@
+//
+//  ViewController.swift
+//  Lab 7 D
+//
+//  Created by layla hakami on 12/03/1443 AH.
+//
+
+import UIKit
+
+
+class ViewController: UIViewController {
+
+    var GoldenStateWarriors = [ "player1" : "Curry", "player2" : "Thomson" , "player3" : "Green"]
+   var Lakers  = [ "player1" : "Lebron", "player2" :  "Westbrook",  "player3" :  "Davis"]
+    
+   var Clipperss  = [ "player1" : "Leornard", "player2" : "Batum",   "player3" :   "Ibaka"]
+   
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        print(" *  the GoldenStateWarriors * ")
+        
+        for (_,value) in GoldenStateWarriors{
+            print ("\(value)")
+            
+        }
+        
+        
+        print(" * the Lakers * ")
+        
+            for (_,value) in Lakers{
+              print ("\(value)")
+        }
+        print(" * the Clipperss * ")
+        
+        for (_,value) in Clipperss{
+          print ("\(value)")
+    }
+ 
+    
+   //print ("The NBA League got the following player : the GoldenStateWarriors \(GoldenStateWarriors(value))   the Lakers  \(Lakers(value))  the Clipperss  \(Clipperss(value)) ")
+
+}
+}
---
+++ Lab77/Lab77/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  Lab77
+//
+//  Created by layla hakami on 11/03/1443 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    let teamPlay1 = ["play1": "Curry", "play2": "Thomson", "play3": "Green"]
+    
+    let teamplay2 = ["play1":"Lebron", "play2":"Westbrook","play3": "Davis"]
+    
+    let teamplay3 = ["play1":"Leornard", "play2": "Batum", "play3":"Ibaka"]
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        print ("\ (key is a\ value)")
+    }
+
+
+}
+
