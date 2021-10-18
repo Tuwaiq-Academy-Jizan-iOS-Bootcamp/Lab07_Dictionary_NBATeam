d6d05033fc703edfdc20311c1e2a55ab4fa1779b
Afaf Yahya <afafyahya@MacBook-Pro-Afaf.local>
2021-10-19 00:22:09 +0300

    lap7

---
+++ Lap7/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  Lap7
+//
+//  Created by Afaf Yahya on 11/03/1443 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    var team1 = ["Curry" : "Golden Statr Warrios", "Thomson" : "Golden Statr Warrios", "Green" : "Golden Statr Warrios"]
+    var team2 = ["Lebron" : "Lakers", "Westbrook" : "Lakers", "Davis": "Lakers"]
+    var team3 = ["Leornard" : "Clippers", "Batum": "Clippers", "Ibaka" :"Clippers"]
+    
+
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
