3892a5d6ba5a615f877b1266365f4a8ccf2f1bc9
Bushra Barakat <bushrabarakat@MacBook-Pro-Bushra.local>
2021-10-18 05:44:53 +0300

    lab07

---
+++ lab07/lab07/ViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  lab07
+//
+//  Created by Bushra Barakat on 11/03/1443 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    let team1 =
+    ["goldenStateWarriors": "Curry", "goldenStateWarriors": "Thomson" , "goldenStateWarriors":"Green"]
+    let team2 =
+    ["laker": "Lebron", "laker": "Westbrook", "laker": "Davi"]
+    let team3 =
+    ["clippers":"Leornard","clippers":"Batum","clippers":"lbaka"]
+     
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+       print ("The NBA Leagus have the following players \(team1) and \(team2) and \(team3)")
+        
+    }
+    
+    
+    
+    
+    
+      
+        
+    }
+    
+    
+
+
