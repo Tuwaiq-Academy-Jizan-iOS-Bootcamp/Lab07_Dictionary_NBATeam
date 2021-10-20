049dec0948e159fafe3182d39726ca16f317212f
Nasser ali <naseeri1983@gmail.com>
2021-10-20 10:25:52 +0300

    don

---
+++ Lab07_Dictionary_NBATeam-main/MyHW7/MyHW7/ViewController.swift
@@ -0,0 +1,23 @@
+
+//  Created by nasser ali salem on 11/03/1443 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    let team1 = ["curry":"Golden State Warrios ","thomson":"Golden state warriors","green":"Golden state warriors"]
+             
+    let team2 = ["Lebron":"Lakers","Westbrook":"Lakers","Davis":"Lakers"]
+             
+ let team3=["Leornard":"Clippers","Batum":"Clippers","lbaka":"Clippers"]
+    
+    
+override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    
+    print("the NBN Leagus have the following players \(team1) and \(team2) and \(team3).")
+}
+           }
+
