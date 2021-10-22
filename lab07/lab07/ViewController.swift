8373b8f0dafea2dea3ae055074052e8737784b40
bushrabarakat1 <Bushra.Barakat.jiz.s@tuwaiq.edu.sa>
2021-10-22 14:17:31 +0300

    Bushra

--- lab07/lab07/ViewController.swift
+++ lab07/lab07/ViewController.swift
@@ -8,18 +8,33 @@
 import UIKit
 
 class ViewController: UIViewController {
-    let team1 =
-    ["goldenStateWarriors": "Curry", "goldenStateWarriors": "Thomson" , "goldenStateWarriors":"Green"]
-    let team2 =
-    ["laker": "Lebron", "laker": "Westbrook", "laker": "Davi"]
-    let team3 =
-    ["clippers":"Leornard","clippers":"Batum","clippers":"lbaka"]
+   var team1 =
+    ["goldenStateWarriors1": "Curry", "goldenStateWarriors2": "Thomson" , "goldenStateWarriors3":"Green"]
+    var team2 =
+    ["laker1": "Lebron", "laker2": "Westbrook", "laker3": "Davi"]
+    var team3 =
+    ["clippers1":"Leornard","clippers2":"Batum","clippers3":"lbaka"]
      
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-       print ("The NBA Leagus have the following players \(team1) and \(team2) and \(team3)")
+        
+        team1["goldenStateWarriors4"] = "ahmad"
+        team2["laker4"] = "omar"
+        team3["clippers4"] = "mohammad"
+        
+        team1.updateValue("Ali", forKey: "goldenStateWarriors1")
+        team2.updateValue("Anas", forKey: "laker2")
+        team3.updateValue("Ameen", forKey: "clippers3")
+        
+        team1.removeValue(forKey: "goldenStateWarriors2")
+        team2.removeValue(forKey: "laker3")
+        team3.removeValue(forKey: "clippers1")
+        
+        
+    print ("\(team1) and \(team2) and \(team3)")
+        
         
     }
     
