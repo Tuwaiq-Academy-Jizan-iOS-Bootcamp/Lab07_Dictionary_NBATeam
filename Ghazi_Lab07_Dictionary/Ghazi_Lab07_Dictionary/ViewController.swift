2e8bb5212f4c18f67cc589ba4ebcd03394394dc4
GhaziShubayli <ghazi.shubayli.jiz.s@tuwaiq.edu.sa>
2021-12-13 14:41:33 +0300

    "Ghazi"

---
+++ Ghazi_Lab07_Dictionary/Ghazi_Lab07_Dictionary/ViewController.swift
@@ -0,0 +1,46 @@
+//
+//  ViewController.swift
+//  Ghazi_Lab07_Dictionary
+//
+//  Created by ماك بوك on 09/05/1443 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        // create three dictionary
+        var goldenStateWarriorsTeam=["Player1":"Curry","Player2":"Thomson","Player3":"Green"]
+        var lakersTeam=[1:"Lebron",2:"Westbrook",3:"Davis"]
+        var clippersTeam=[1:"Leornard",2:"Batum",3:"Ibaka"]
+        //print the three dictionary
+        print(lakersTeam)
+        print(goldenStateWarriorsTeam)
+        print(clippersTeam)
+        //print a specific item
+        print(lakersTeam[2]!)
+        //another way
+        let playerOneClippers=clippersTeam[1]
+        print(playerOneClippers!)
+        //item update
+        lakersTeam[2] = "Ali"
+        print(lakersTeam)
+        //another way
+        clippersTeam.updateValue("Mohammad", forKey: 1)
+        print(clippersTeam)
+        //remove item
+        goldenStateWarriorsTeam.removeValue(forKey: "Player3")
+        print(goldenStateWarriorsTeam)
+        //print the uumber of index items
+        print(clippersTeam.count)
+        
+    }
+   
+}
+
+
+
